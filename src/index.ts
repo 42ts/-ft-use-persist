df1401ceb19e8011a9b6db09a4e0f0196b034466
Juyeong Maing <mjy9088@naver.com>
2023-09-13 23:44:25 +0900

    add function signature

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,21 @@ export interface UsePersist<T> {
   current: T;
 }
 
+export function usePersist<T>(): UsePersist<T | undefined>;
 export function usePersist<T>(
   defaultValue: (() => T) | (T extends (...args: any[]) => any ? never : T)
-): UsePersist<T> {
+): UsePersist<T>;
+export function usePersist<T>(
+  defaultValue?: (() => T) | (T extends (...args: any[]) => any ? never : T)
+) {
   const result: UsePersist<[UsePersist<T>?]> = useRef([]);
   if (!result.current[0])
     result.current = [
       {
         current:
-          defaultValue instanceof Function ? defaultValue() : defaultValue,
+          defaultValue instanceof Function
+            ? defaultValue()
+            : (defaultValue as T),
       },
     ];
   return result.current[0]!;
